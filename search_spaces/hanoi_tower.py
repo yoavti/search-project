71d0da964ec2ccb7e89358f166d9f0fbec8dffa6
yoavti <yoavti@post.bgu.ac.il>
2022-02-21 20:10:58 +0200

    Reimplemented Hanoi

--- search_spaces/hanoi_tower.py
+++ search_spaces/hanoi_tower.py
@@ -1,8 +1,6 @@
-from typing import List, Tuple, Set
 import numpy as np
-
-from algorithms import UCS
-from search_spaces import SearchSpace
+from algorithms.ucs import UCS
+from search_spaces.search_space import SearchSpace
 from state import State
 import copy
 
@@ -13,67 +11,62 @@ def first_nonzero(arr, invalid_val=-1):
 
 
 class HanoiTower(SearchSpace):
-    def __init__(self, disks=int(input("enter how many disks to put on the peg "))):
+    def __init__(self, pegs, disks):
+        assert pegs >= 3
+        assert disks >= 2
+        self.pegs = pegs
         self.disks = disks
-        hanoi_towers = np.zeros((10, self.disks))
-        first_tower = np.array(list(range(1, self.disks + 1)))
+        hanoi_towers = [set() for _ in range(self.pegs)]
+        first_tower = set(range(self.disks))
         hanoi_towers[0] = first_tower
         self.start_state = State(hanoi_towers)
         self.h_dict = {}
 
-    def get_start(self) -> State:
-        """Returns the start state for the search"""
+    def get_start(self):
         return self.start_state
 
     def generate_h(self):
         self.h_dict = UCS(self).all_states()
 
-    def is_goal(self, s: State) -> bool:
-        """Returns whether the given state is a goal"""
-        hanoi_towers_sorted = np.zeros((10, self.disks))
-        last_tower = np.array(list(range(1, self.disks + 1)))
-        hanoi_towers_sorted[9] = last_tower
-        return np.array_equal(s.data, hanoi_towers_sorted)
+    def calc_smallest_disc(self, s, peg):
+        tower = s.data[peg]
+        if len(tower) > 0:
+            return min(tower)
+        return self.disks
 
-    def get_neighbors(self, s: State) -> List[Tuple[State, float]]:
-        """Returns all neighbor states of the given state, as well as the cost to reach them"""
+    def get_neighbors(self, s):
         neighbors = []
-        for i in range(10):
-            j = first_nonzero(s.data[i], 0)
-            if j != -1:
-                for k in range(0, 9):
-                    if k != i:
-                        non_zero_index = first_nonzero(s.data[k])
-                        if non_zero_index != -1:
-                            if s.data[i][j] < s.data[k][non_zero_index]:
-                                non_zero_index -= 1
-                            else:
-                                continue
-                        hanoi_copy = copy.deepcopy(s.data)
-                        hanoi_copy[k][non_zero_index] = s.data[i][j]
-                        hanoi_copy[i][j] = 0
-                        neighbors.append((State(hanoi_copy), 1))
+        smallest_discs = [self.calc_smallest_disc(s, peg) for peg in range(self.pegs)]
+        for src in range(self.pegs):
+            src_smallest = smallest_discs[src]
+            if src_smallest == self.disks:
+                continue
+            for dst in range(self.pegs):
+                if src == dst:
+                    continue
+                dst_smallest = smallest_discs[dst]
+                if src_smallest >= dst_smallest:
+                    continue
+                to_add = copy.deepcopy(s.data)
+                to_add[src].remove(src_smallest)
+                to_add[dst].add(src_smallest)
+                neighbors.append((State(to_add), 1))
         return neighbors
 
-    def h(self, s: State) -> float:
-        """Returns the heuristic value of the given state"""
+    def h(self, s):
         return self.h_dict[s]
 
-    def h_cap(self, s: State) -> float:
-        """Returns the heuristic value of the given state as if all operator costs were 1"""
+    def h_cap(self, s):
         return self.h_dict[s]
 
-    def h_cost_adapted(self, c: float, s: State):
-        """The same algorithm to compute h, but adds a constant c to each operator cost."""
+    def h_cost_adapted(self, c, s):
         return self.h_dict[s] + c
 
-    def operator_costs(self) -> Set[float]:
-        """Returns a set of all operator costs"""
-        costs = set()
-        costs.add(1)
-        return costs
+    def operator_costs(self):
+        return {1}
 
-    def get_goal(self) -> State:
-        hanoi_towers_sorted = np.zeros((10, self.disks))
-        last_tower = np.array(list(range(1, self.disks + 1)))
+    def get_goal(self):
+        hanoi_towers_sorted = [set() for _ in range(self.pegs)]
+        last_tower = set(range(self.disks))
+        hanoi_towers_sorted[-1] = last_tower
         return State(hanoi_towers_sorted)
