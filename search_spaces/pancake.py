92db12ca14e7f82fccc4a16d4c77b974f65624c9
yoavti <yoavti@post.bgu.ac.il>
2022-02-21 20:10:44 +0200

    Added shuffle_start

--- search_spaces/pancake.py
+++ search_spaces/pancake.py
@@ -1,14 +1,12 @@
-from typing import List, Tuple, Set
+from typing import List
 import random
-
-from algorithms import UCS
-from search_spaces import SearchSpace
+from algorithms.ucs import UCS
+from search_spaces.search_space import SearchSpace
 from state import State
-import numpy as np
 import copy
 
 
-def flip(array, i) -> List[int]:
+def flip(array, i: int) -> List[int]:
     start = 0
     arr = copy.deepcopy(array)
     while start < i:
@@ -21,49 +19,40 @@ def flip(array, i) -> List[int]:
 
 
 class Pancake(SearchSpace):
-    def __init__(self, graph_size=int(input("enter pancake problem size "))):
+    def __init__(self, graph_size):
+        assert graph_size >= 2
         self.graph_size = graph_size
-        pancake_array = list(range(1, self.graph_size + 1))
-        random.shuffle(pancake_array)
-        self.start_state = State(pancake_array)
+        self.start_state = State(list(range(1, self.graph_size + 1)))
+        self.shuffle_start()
         self.h_dict = {}
 
-    def get_start(self) -> State:
-        """Returns the start state for the search"""
+    def shuffle_start(self):
+        random.shuffle(self.start_state.data)
+
+    def get_start(self):
         return self.start_state
 
     def generate_h(self):
         self.h_dict = UCS(self).all_states()
 
-    def is_goal(self, s: State) -> bool:
-        """Returns whether the given state is a goal"""
-        return np.array_equal(s.data, np.array(list(range(1, self.graph_size + 1))))
-
-    def get_neighbors(self, s: State) -> List[Tuple[State, float]]:
-        """Returns all neighbor states of the given state, as well as the cost to reach them"""
+    def get_neighbors(self, s):
         neighbors = []
         for i in range(1, self.graph_size):
             neighbors.append((State(flip(s.data, i)), 1))
         return neighbors
 
-    def h(self, s: State) -> float:
-        """Returns the heuristic value of the given state"""
+    def h(self, s):
         return self.h_dict[s]
 
-    def h_cap(self, s: State) -> float:
-        """Returns the heuristic value of the given state as if all operator costs were 1"""
+    def h_cap(self, s):
         return self.h_dict[s]
 
-    def h_cost_adapted(self, c: float, s: State):
-        """The same algorithm to compute h, but adds a constant c to each operator cost."""
+    def h_cost_adapted(self, c, s):
         return self.h_dict[s] + c
 
-    def operator_costs(self) -> Set[float]:
-        """Returns a set of all operator costs"""
-        costs = set()
-        costs.add(1)
-        return costs
+    def operator_costs(self):
+        return {1}
 
-    def get_goal(self) -> State:
+    def get_goal(self):
         pancake_array = list(range(1, self.graph_size + 1))
         return State(pancake_array)
