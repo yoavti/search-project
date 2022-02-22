7e79187367baacf5d96f981417936e6d07680d5c
yoavti <yoavti@post.bgu.ac.il>
2022-02-22 11:08:47 +0200

    made it so generate_h is called once for each search space parameter

--- main.py
+++ main.py
@@ -7,72 +7,54 @@ from search_spaces.hanoi_tower import HanoiTower
 from search_spaces.pancake import Pancake
 
 
-def start_experiment(search_space, algorithm_constructor):
+MULT_FACTOR = 2
+
+
+def initialize_search_space(search_space):
     operator_costs = search_space.operator_costs()
-    epsilon = min(operator_costs) / 2
-    M = max(operator_costs) * 2
-    algorithm = algorithm_constructor(search_space, epsilon, M)
-    print(algorithm.__class__.__name__)
+    epsilon = min(operator_costs) / MULT_FACTOR
+    M = max(operator_costs) * MULT_FACTOR
     print('generating h*')
     search_space.generate_h()
     print('finished')
-    return algorithm
+    return epsilon, M
 
 
-def pancake_experiment(graph_size, algorithm_constructor, num_runs=1, print_runs=False):
-    search_space = Pancake(graph_size)
-    algorithm = start_experiment(search_space, algorithm_constructor)
-    runtime_sum = 0
-    expanded_states_sum = 0
-    for i in range(num_runs):
-        search_space.reset_start()
+def compare_algorithms(search_space, algorithm_constructors):
+    epsilon, M = initialize_search_space(search_space)
+    for algorithm_constructor in algorithm_constructors:
+        algorithm = algorithm_constructor(search_space, epsilon, M)
+        print(algorithm.__class__.__name__)
         runtime, expanded_states = algorithm.until_goal()
-        if print_runs:
-            print(i)
-            print(search_space.get_start().data)
-            print('runtime:', runtime)
-            print('expanded_states:', expanded_states)
-        runtime_sum += runtime
-        expanded_states_sum += expanded_states
-    print('avg runtime:', runtime_sum / num_runs)
-    print('avg expanded_states:', expanded_states_sum / num_runs)
-
-
-def hanoi_experiment(pegs, disks, algorithm_constructor):
-    search_space = HanoiTower(pegs, disks)
-    algorithm = start_experiment(search_space, algorithm_constructor)
-    runtime, expanded_states = algorithm.until_goal()
-    print('runtime:', runtime)
-    print('expanded_states:', expanded_states)
+        print('runtime:', runtime)
+        print('expanded_states:', expanded_states)
+        print()
+    print()
 
 
 def pancake_experiments(sizes, algorithm_constructors):
     for graph_size in sizes:
         print(f'{graph_size=}')
-        for algorithm_constructor in algorithm_constructors:
-            pancake_experiment(graph_size, algorithm_constructor)
-            print()
-        print()
+        search_space = Pancake(graph_size)
+        compare_algorithms(search_space, algorithm_constructors)
 
 
 def hanoi_experiments(peg_disk_combinations, algorithm_constructors):
     for pegs, disks in peg_disk_combinations:
         print(f'{pegs=} {disks=}')
-        for algorithm_constructor in algorithm_constructors:
-            hanoi_experiment(pegs, disks, algorithm_constructor)
-            print()
-        print()
+        search_space = HanoiTower(pegs, disks)
+        compare_algorithms(search_space, algorithm_constructors)
 
 
 def non_cost_adapted(algorithm):
-    def ret(search_space, epsilon, M):
+    def ret(search_space, _, __):
         return algorithm(search_space)
 
     return ret
 
 
 def epsilon_cost_adapted(algorithm):
-    def ret(search_space, epsilon, M):
+    def ret(search_space, epsilon, _):
         print('epsilon')
         return algorithm(search_space, epsilon)
 
@@ -80,7 +62,7 @@ def epsilon_cost_adapted(algorithm):
 
 
 def M_cost_adapted(algorithm):
-    def ret(search_space, epsilon, M):
+    def ret(search_space, _, M):
         print('M')
         return algorithm(search_space, M)
 
@@ -88,15 +70,15 @@ def M_cost_adapted(algorithm):
 
 
 if __name__ == '__main__':
-    algorithm_constructors = [non_cost_adapted(AStar),
-                              non_cost_adapted(HCapTieBreaking),
-                              non_cost_adapted(HTieBreaking),
-                              epsilon_cost_adapted(FCostAdaptedTieBreaking),
-                              epsilon_cost_adapted(HCostAdaptedTieBreaking),
-                              M_cost_adapted(FCostAdaptedTieBreaking),
-                              M_cost_adapted(HCostAdaptedTieBreaking)]
-    peg_disk_combinations_backup = [(3, 10), (4, 8), (5, 6), (6, 5), (7, 5), (8, 5), (9, 5), (10, 5)]
-    peg_disk_combinations = [(3, 10), (4, 8), (5, 6)]
-    hanoi_experiments(peg_disk_combinations, algorithm_constructors)
-    sizes = list(range(2, 9))
-    pancake_experiments(sizes, algorithm_constructors)
+    _algorithm_constructors = [non_cost_adapted(AStar),
+                               non_cost_adapted(HCapTieBreaking),
+                               non_cost_adapted(HTieBreaking),
+                               epsilon_cost_adapted(FCostAdaptedTieBreaking),
+                               epsilon_cost_adapted(HCostAdaptedTieBreaking),
+                               M_cost_adapted(FCostAdaptedTieBreaking),
+                               M_cost_adapted(HCostAdaptedTieBreaking)]
+    _peg_disk_combinations_backup = [(3, 10), (4, 8), (5, 6), (6, 5), (7, 5), (8, 5), (9, 5), (10, 5)]
+    _peg_disk_combinations = [(3, 10), (4, 8), (5, 6)]
+    hanoi_experiments(_peg_disk_combinations, _algorithm_constructors)
+    _sizes = list(range(2, 10))
+    pancake_experiments(_sizes, _algorithm_constructors)
